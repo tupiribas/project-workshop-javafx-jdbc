4d7e8711c41f8ca41f28364b0090b390227bb374
Tupi Guedes <tupiguedes@outlook.com>
2021-08-28 22:22:01 -0300

    About view

---
+++ src/gui/util/Alerts.java
@@ -0,0 +1,16 @@
+package gui.util;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class Alerts {
+
+	public static void showAlert(String title, String header, String content, AlertType alertType) {
+		Alert alert = new Alert(alertType);
+		alert.setTitle(title);
+		alert.setHeaderText(header);
+		alert.setContentText(content);
+		alert.setAlertType(alertType);
+		alert.show();
+	}
+}
