12f29eb63e71815e010ff2b95636972868c5ee10
Tupi Guedes <tupiguedes@outlook.com>
2021-08-28 21:17:16 -0300

    Main view controller

--- src/module-info.java
+++ src/module-info.java
@@ -2,5 +2,8 @@ module project_workshop_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
 	
+	exports gui;
+	opens gui to javafx.fxml;
+	
 	opens application to javafx.graphics, javafx.fxml;
 }
