64395dcce4ea9d6d142164e63cffbc5079e75f8d
Tupi Guedes <tupiguedes@outlook.com>
2021-09-03 03:52:59 -0300

    DepartmentForm (dialog) design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -36,8 +45,9 @@ public class DepartmentListController implements Initializable {
 	private ObservableList<Department> obsDepartmentList;
 
 	@FXML
-	public void onButtonNewAction() {
-		System.out.println("onButtonNewAction");
+	public void onButtonNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 	}
 
 	public void setDepartmentService(DepartmentService departmentService) {
@@ -62,7 +72,8 @@ public class DepartmentListController implements Initializable {
 	public void uppdateTableView() {
 		if (departmentService == null) {
 			throw new IllegalStateException("Defect cod.:02>>> service was null");
-		} else {
+		} 
+		else {
 			/*
 			 * Cria uma lista local e atribui a ela os mocks criados na classe
 			 * DepartmentService
@@ -74,6 +85,24 @@ public class DepartmentListController implements Initializable {
 
 			// Pega os valores obsDepartmentList e joga para a tableViewDepartment
 			tableViewDepartment.setItems(obsDepartmentList);
+		}
+	}
+
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		} 
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", null, "Defect cod.:02>>> loading view", AlertType.ERROR);
 		}
 	}
 
